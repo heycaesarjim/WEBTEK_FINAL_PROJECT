125610c34e44297d9564262f712435b4ae4d7cb4
DarrenS <darren_sison2026@yahoo.com>
2018-05-01 20:18:00 +0800

    Quiz for the webtech websites

---
+++ course/course/quiz/quiz2.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* quiz1.php
+*
+* Webtech quiz number 1
+* 
+* @author Darren Sison
+*/ 
+require 'classes/UserAccount.php';
+<form action="quiz_result2.php" method="post" id="quiz">
+<li>
+	<h3>HTML Stands for...</h3>
+	<div>
+		<input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+		<label for="question-1-answers-A">A) Hypertext Markup Language </label>
+	</div>
+	<div>
+		<input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+		<label for="question-1-answers-B">B) Hypertext Markup</label>
+	</div>
+	<div>
+		<input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+		<label for="question-1-answers-C">C) Hypertext Programming</label>
+	</div>
+	<div>
+		<input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+		<label for="question-1-answers-D">D) None of the above</label>
+	</div>
+</li>
+</form>
+<input type="submit" value="Submit Quiz" />
+?>
